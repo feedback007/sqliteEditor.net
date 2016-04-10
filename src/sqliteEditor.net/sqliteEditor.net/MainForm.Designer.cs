07dd1f094b1e3198bce37d7523cc6ed8873c6844
feedback007 <dinglj1760@163.com>
2016-04-10 20:08:41 +0800

    first create

---
+++ src/sqliteEditor.net/sqliteEditor.net/MainForm.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace sqliteEditor.net
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.actionList1 = new Crad.Windows.Forms.Actions.ActionList();
+            this.vS2013BlueTheme1 = new WeifenLuo.WinFormsUI.Docking.VS2013BlueTheme();
+            this.dockPanel1 = new WeifenLuo.WinFormsUI.Docking.DockPanel();
+            this.statusBar = new System.Windows.Forms.StatusStrip();
+            this.vsToolStripExtender1 = new sqliteEditor.net.VSToolStripExtender(this.components);
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.actionList1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(612, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(612, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // actionList1
+            // 
+            this.actionList1.ContainerControl = this;
+            // 
+            // dockPanel1
+            // 
+            this.dockPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dockPanel1.Location = new System.Drawing.Point(0, 49);
+            this.dockPanel1.Name = "dockPanel1";
+            this.dockPanel1.Size = new System.Drawing.Size(612, 324);
+            this.dockPanel1.TabIndex = 5;
+            // 
+            // statusBar
+            // 
+            this.statusBar.Location = new System.Drawing.Point(0, 373);
+            this.statusBar.Name = "statusBar";
+            this.statusBar.RenderMode = System.Windows.Forms.ToolStripRenderMode.ManagerRenderMode;
+            this.statusBar.Size = new System.Drawing.Size(612, 22);
+            this.statusBar.TabIndex = 7;
+            this.statusBar.Text = "statusStrip1";
+            // 
+            // vsToolStripExtender1
+            // 
+            this.vsToolStripExtender1.DefaultRenderer = null;
+            this.vsToolStripExtender1.VS2012Renderer = null;
+            this.vsToolStripExtender1.VS2013Renderer = null;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 395);
+            this.Controls.Add(this.dockPanel1);
+            this.Controls.Add(this.statusBar);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.KeyPreview = true;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "sqliteEditor";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.actionList1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private Crad.Windows.Forms.Actions.ActionList actionList1;
+        private WeifenLuo.WinFormsUI.Docking.VS2013BlueTheme vS2013BlueTheme1;
+        private WeifenLuo.WinFormsUI.Docking.DockPanel dockPanel1;
+        private System.Windows.Forms.StatusStrip statusBar;
+        private VSToolStripExtender vsToolStripExtender1;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ src/sqliteEditor.net/sqliteEditor.net/MainForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using sqliteEditor.net.UI;
+
+namespace sqliteEditor.net
+{
+    public partial class MainForm : Form
+    {
+        SQLEditorForm _SQLEditorForm = null;
+        CatalogTreeForm _dbTreeForm = null;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            _SQLEditorForm = new SQLEditorForm();
+            _SQLEditorForm.DockPanel = this.dockPanel1;
+            _SQLEditorForm.Show();
+
+            _dbTreeForm = new CatalogTreeForm();
+            _dbTreeForm.DockPanel = this.dockPanel1;
+            _dbTreeForm.Show();
+        }
+    }
+}
